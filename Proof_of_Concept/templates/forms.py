dd1ae62a3ded18ceeea83472e82ed2d756d3e8ef
John Lam <57305641+JohnLam916@users.noreply.github.com>
2021-04-16 22:09:35 -0700

    Delete Proof_of_Concept directory

--- Proof_of_Concept/templates/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class login_form(FlaskForm):
-    api_key =StringField("Please enter your API_Key")
-    api_secret = StringField("Please enter your API_Secret")
-    submit = SubmitField("I want to be a Winner")

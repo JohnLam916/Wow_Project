e805dd29be9dc2c15a67eaee7d24f7bacd3dbe5b
JohnLam916 <johnvenhlam@gmail.com>
2021-04-16 22:18:22 -0700

    had to do some file reorganizing

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 

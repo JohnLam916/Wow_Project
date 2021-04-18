7de18111a2e25e7b28357acbb3c89b5679a4c6f1
JohnLam916 <johnvenhlam@gmail.com>
2021-04-18 08:32:33 -0700

    Obtaining user input

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template
+#Validating my client key and allow me to access Binance data stream
+from binance.client import Client
+
+#Allows me to make trades
+from binance.enums import *
+
+from flask import Flask, render_template, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 if __name__ == "__main__":
 
-#   putting a parameter of debug=True in app.run() will automatically put our flask in debug mode and automatically update any changes we make to flask
+# Putting a parameter of debug=True in app.run() will automatically put our flask in debug mode and automatically update any changes we make to flask
     app.run(debug=True)
 
 # the Post and GET method will allow us to retrieve information from the page
 @app.route('/', methods=["POST", "GET"])
-def login():
-    return render_template('login.html')
 
+# Utilizing request library to store information from my login form
+def login():
+
+    if request.method == "POST":
+        key = request.form["key"]
+        secret = request.form["secret"]
+
+        client = Client(key, secret, tld = 'us')
+
+        account_info = client.get_account()
+
+        print(account_info)
+
+        return redirect(url_for("index"))
+
+    else: 
+        return render_template("login.html")
+    
 @app.route('/index')
 def index():
     return render_template('index.html')

2dfa0031ef912a0c1800c8518a2a2ad3e2281cd9
JohnLam916 <johnvenhlam@gmail.com>
2021-04-23 08:53:50 -0700

    Cleaning up files and added documentation

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
 # Putting a parameter of debug=True in app.run() will automatically put our flask in debug mode and automatically update any changes we make to flask
     
-# the Post and GET method will allow us to retrieve information from the page
+# The Post and GET method will allow us to retrieve information from the page
 @app.route('/', methods=["POST", "GET"])
 def login():
 
@@ -59,18 +59,23 @@ def login():
     else: 
         return render_template("login.html")
 
+# Route where our main page is located
 @app.route('/index', methods=["POST", "GET"])
 def index():
 
     return render_template('index.html')
 
+# Under construction
 @app.route("/logout")
 def logout():
     session.pop("key", None)
     return redirect(url_for("login"))
 
+# Route where our Binance buy method is housed
 @app.route('/buy', methods=["POST","GET"])
 def buy():
+
+        # If a Post request was submitted through the buy form the following will code the Buy operation
         if request.method == "POST":
                 try:
                 
@@ -85,38 +90,47 @@ def buy():
                         type=ORDER_TYPE_MARKET,
                         quantity= float(request.form['quantity']))
 
+                        #Connecting to user's binance account to retrieve information about account balances 
                         exchange_info = client.get_exchange_info()
                         info = client.get_account()
                         balances = info['balances']
                         available_to_trade = info['balances'][2]['free']
 
+                        # Retrieve which symbol is selected for buying
                         symbols = exchange_info['symbols']
+
+                        # Displays the current prices of Crypto currencies
                         prices = client.get_all_tickers()
-
-
                 
                 except Exception as e:
                         flash(e.message, "error")
 
-                return render_template('index.html', my_balances=balances, available_to_trade=available_to_trade, prices=prices, symbols=symbols)     
+                # Once the order executes, the page refreshes and updates the available balance to trade, current holdings, and prices of crypto currencies
+                return render_template('index.html', my_balances=balances, available_to_trade=available_to_trade, 
+                prices=prices, symbols=symbols)     
         else: 
+                # If a trade has not been successfully executed the user is returned the same page
                 return render_template("login.html")  
         
-
+# Route where our Binance sell method is housed
 @app.route('/sell', methods=["POST","GET"])
 def sell():
+
+        # If a Post request was submitted through the sell form the following will code will execute
         if request.method == "POST":
+
                 # Utilizing session to make key accessible to the sell route, to enable user to make a trade via binance
                 key = session["key"]
                 secret = session["secret"]
                 client = Client(key, secret, tld = 'us')
                 
-                # Function to make an trade 
+                # Function to make a sell 
                 order = client.create_order(symbol=request.form['symbol'],
                 side=SIDE_SELL,
                 type=ORDER_TYPE_MARKET,
                 quantity= float(request.form['quantity']))
 
+                # Once a sell order has been successfully executed, the users available balance is retrieved to be displayed on the page
                 exchange_info = client.get_exchange_info()
                 info = client.get_account()
                 
@@ -124,15 +138,18 @@ def sell():
                 available_to_trade = info['balances'][2]['free']
                 symbols = exchange_info['symbols']
 
+                # Displays the current prices of the Crypto currencys
                 prices = client.get_all_tickers()
-
                 print(request.form)
 
+                # Once the order executes, the page refreshes and updates the available balance to trade, current holdings, and prices of crypto currencies
                 return render_template('index.html', my_balances=balances, available_to_trade=available_to_trade, prices=prices, symbols=symbols)       
 
         else: 
-                return render_template("login.html")  
+                # If a trade is unsuccessful it refreshes back to the same page
+                return render_template("index.html")  
 
+# Under construction, this will house the function that will allow a user to set parameters for the bot to trade
 @app.route('/settings')
 def settings():
     return 'settings'

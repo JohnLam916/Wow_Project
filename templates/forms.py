1b926116bf2209bb25f78a0cdb86423f486a0f4d
John Lam <57305641+JohnLam916@users.noreply.github.com>
2021-04-24 10:05:45 -0700

    Delete forms.py

--- templates/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    api_key = StringField('Please enter your api_key', validators=[DataRequired()])
-    api_secret = PasswordField('Please enter your api_secret', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('I want to be a Winner')
